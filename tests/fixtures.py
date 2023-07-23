cb9532d9ad6fb66b1f10b9bc749ca54d068f9327
Elliot Morris <elliot.morris@foundry.com>
2023-07-23 17:03:27 +0100

    [Core] Update to Openassetio 13
    Make the neccesary changes to update to recent versions of
    openassetio and openassetio-mediacreation
    
    Signed-off-by: Elliot Morris <elliot.morris@foundry.com>

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,7 +1,7 @@
 """
 Manager test harness test case fixtures for My Asset Manager.
 """
-from openassetio.constants import kField_EntityReferencesMatchPrefix
+from openassetio import constants
 from openassetio_mediacreation.traits.content import LocatableContentTrait
 
 
@@ -22,27 +22,24 @@ fixtures = {
     "shared": {
         "a_valid_reference": VALID_REF,
         "an_invalid_reference": NON_REF,
-        "a_malformed_reference": MALFORMED_REF,
     },
     "Test_identifier": {"test_matches_fixture": {"identifier": IDENTIFIER}},
     "Test_displayName": {"test_matches_fixture": {"display_name": "My Asset Manager"}},
     "Test_info": {
         "test_matches_fixture": {
-            "info": {kField_EntityReferencesMatchPrefix: "my_asset_manager:///"}
+            "info": {constants.kField_EntityReferencesMatchPrefix: "my_asset_manager:///"}
         }
     },
     "Test_resolve": {
-        "shared": {
-            "a_reference_to_a_readable_entity": EXISTING_REF,
-            "a_set_of_valid_traits": {LocatableContentTrait.kId},
-            "a_reference_to_a_readonly_entity": EXISTING_REF,
-            "the_error_string_for_a_reference_to_a_readonly_entity": "Entities are read-only",
-            "a_reference_to_a_missing_entity": "my_asset_manager:///missing_entity",
-            "the_error_string_for_a_reference_to_a_missing_entity": (
-                "Entity 'my_asset_manager:///missing_entity' not found"
-            ),
-            "a_malformed_reference": MALFORMED_REF,
-            "the_error_string_for_a_malformed_reference": "Entity identifier is malformed",
-        }
+        "a_reference_to_a_readable_entity": EXISTING_REF,
+        "a_set_of_valid_traits": {LocatableContentTrait.kId},
+        "a_reference_to_a_readonly_entity": EXISTING_REF,
+        "the_error_string_for_a_reference_to_a_readonly_entity": "Entities are read-only",
+        "a_reference_to_a_missing_entity": "my_asset_manager:///missing_entity",
+        "the_error_string_for_a_reference_to_a_missing_entity": (
+            "Entity 'my_asset_manager:///missing_entity' not found"
+        ),
+        "a_malformed_reference": MALFORMED_REF,
+        "the_error_string_for_a_malformed_reference": "Entity identifier is malformed",
     },
 }
